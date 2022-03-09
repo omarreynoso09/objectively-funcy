fd3e4cad78420f870d951ba77120e9e0caff88e5
Hansel Reynoso <omarreynoso09@gmail.com>
2022-03-09 17:42:23 -0500

    forgot to add this

--- main.js
+++ main.js
@@ -1,4 +1,64 @@
 // Your code here.
+const getFirstName = function (person) {
+  return person.firstName;
+};
+
+
+const getLastName = function (person) {
+  return person.lastName; 
+};
+
+const getFullName = function (person) {
+
+return (person.firstName + " " + person.lastName);
+
+};
+
+const setFirstName = function (person, name) {
+  return (person.firstName = name);
+};
+
+const setAge = function (person, age) {
+  return (person.age = age);
+};
+
+function giveBirthday(person) {
+	if (person["age"] === undefined) {
+		return (person["age"] = 1);
+	} else {
+		return (person["age"] += 1);
+	}
+}
+
+
+function marry(person1, person2) {
+  person1.married = true;
+	person2.married = true;
+	person1.spouseName = person2.firstName + " " + person2.lastName;
+  person2.spouseName = person1.firstName + " " + person1.lastName;
+}
+
+function divorce(person1, person2) {
+	 delete person1.spouseName;
+	 delete person2.spouseName;
+	person1.married = false;
+	person2.married = false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
